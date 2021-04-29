e5ec3abf7c20846ff1e88f25f24228d65876a168
Calvin Edwards <calvin@sentryhealth.ca>
2021-04-29 11:49:33 -0700

    - stats reporting

--- tf.dockerfile
+++ tf.dockerfile
@@ -1,4 +1,4 @@
-FROM hashicorp/terraform:0.12.30
+FROM hashicorp/terraform:0.12.31
 
 ARG project_id=sentry-health-meet
 ARG env_dir=prod
--- tf/cluster.tf
+++ tf/cluster.tf
@@ -1,39 +1,57 @@
-resource "google_service_account" "default" {
-  account_id   = "meet-engine"
-  display_name = "Meet Engine Service Account"
-}
+# # data "google_compute_default_service_account" "default" {
+# # }
 
-resource "google_container_cluster" "primary" {
-  name     = "meet-cluster"
-  location = var.zone
+# resource "google_service_account" "default" {
+#   account_id   = "meet-engine"
+#   display_name = "Meet Engine Service Account"
+# }
 
-  enable_shielded_nodes = true
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
+# data "google_iam_policy" "storage_admin" {
+#   binding {
+#     role = "roles/storage.objectAdmin"
 
-resource "google_container_node_pool" "nodes" {
-  name       = "meet-pool"
-  location   = var.zone
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
+#     members = [
+#       "serviceAccount:${google_service_account.default.email}"
+#     ]
+#   }
+# }
+
+# resource "google_service_account_iam_policy" "admin-account-iam" {
+#   service_account_id = google_service_account.default.id
+#   policy_data        = data.google_iam_policy.storage_admin.policy_data
+# }
+
+# resource "google_container_cluster" "primary" {
+#   name     = "meet-cluster"
+#   location = var.zone
+
+#   enable_shielded_nodes = true
+#   # We can't create a cluster with no node pool defined, but we want to only use
+#   # separately managed node pools. So we create the smallest possible default
+#   # node pool and immediately delete it.
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+# }
+
+# resource "google_container_node_pool" "nodes" {
+#   name       = "meet-pool"
+#   location   = var.zone
+#   cluster    = google_container_cluster.primary.name
+#   node_count = 1
   
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 2
-  }
+#   autoscaling {
+#     min_node_count = 0
+#     max_node_count = 2
+#   }
 
-  node_config {
-    preemptible  = false
-    machine_type = "e2-standard-4"
+#   node_config {
+#     preemptible  = false
+#     machine_type = "e2-standard-4"
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+# #    service_account = google_service_account.default.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
